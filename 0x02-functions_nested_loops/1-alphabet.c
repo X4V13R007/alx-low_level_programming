3f25f99f409288658684445d975d6f669d9f2ac8
Asemota Francis <francisasemota.fa@gmail.com>
2022-07-05 11:54:31 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lower case
+ *
+ * created : FrancisXavier
+ * cc: 5 july, 2022
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int ii;
+
+	for (ii = 'a'; ii <= 'z'; ii++)
+		_putchar(ii);
+	_putchar('\n');
+	return;
+}
