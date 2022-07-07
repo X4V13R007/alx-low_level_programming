06bc6a50cff8442ccbcd00123bd20191dfdc3191
Asemota Francis <francisasemota.fa@gmail.com>
2022-07-07 12:46:29 -0700

    Nested loop for printing numbers

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * _isdigit - function that checks for uppercase character.
- * @c: the int for the paramaters of my function
- * Return: 0
+ * _isdigit - Checks for a digit (0 through 9)
+ * @c: The character to check
+ *
+ * Return: 1 if c is a digit. 0 otherwise
  */
-int _isdigit(int x)
+int _isdigit(int c)
 {
-	if (x >= 48 && x <= 57)
-	{
+	if (c >= '0' && c <= '9')
 		return (1);
-	}
-	else
-		return (0);
+
+	return (0);
 }
